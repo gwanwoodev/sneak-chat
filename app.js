98151d4d36c536b71c415babbf0949123c8a79b8
gwanwoodev <gwanwoodev@gmail.com>
2019-06-13 09:27:29 +0900

    update index.pug  & create main.css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const exress = require('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 
@@ -9,6 +9,7 @@ const APP_PORT = 5733
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
    res.render('index');
