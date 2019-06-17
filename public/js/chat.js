44f2dbfa84b2840a585c343170219618c265905f
gwanwoodev <gwanwoodev@gmail.com>
2019-06-17 09:01:37 +0900

    enter key bug fix

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,15 @@
 const socket = io();
+
+socket.on('chatter', function(message) {
+    $('#chat-messages').append($('<li>').text(message));
+    $("#message").val("");
+    $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+});
+
 $('form').submit(function() {
     const name = $("#name").val();
     const message = $("#message").val();
     const dateString = getHoursAndMinutes();
     socket.emit('chatter', `[${dateString}] : ${name} : ${message}`);
     return false;
-});
-
-socket.on('chatter', function(message) {
-    $('#chat-messages').append($('<li>').text(message));
-    $("#message").val("");
-    $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
 });
