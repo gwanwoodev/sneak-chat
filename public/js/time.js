f44453f2a79327a07332f0151a9e60d0106de5d3
gwanwoodev <gwanwoodev@gmail.com>
2019-06-17 09:19:22 +0900

    update time.js morning & afternoon

--- public/js/time.js
+++ public/js/time.js
@@ -2,7 +2,13 @@ function getHoursAndMinutes() {
     let date = new Date(Date.now());
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    let dateString = `${hours}:${minutes}`;
+    const seconds = date.getSeconds();
+    let mornoon;
+    
+    if(hours <= 12) mornoon = '오전';
+    else mornoon = '오후';
+    
+    let dateString = `${mornoon} ${hours}:${minutes}:${seconds}`;
     
     return dateString;
 }
