1a6f78b35b72d442743907b5e92c3204802b14cd
gwanwoodev <gwanwoodev@gmail.com>
2019-06-17 16:07:53 +0900

    db folder move

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('public/db/database.db');
-
-db.serialize(() => {
-   db.each('SELECT nickname from user', (err, row) => {
-      console.log(row.nickname);
-   });
-});
 
 const APP_PORT = 5733
+const db = new sqlite.Database('db/database.db');
 
 const server = app.listen(APP_PORT, () => {
    console.log(`App running on port ${APP_PORT}`);
