23aac0625b30c3d8a8ff306919066efa154cfe3f
gwanwoodev <gwanwoodev@gmail.com>
2019-06-17 10:35:49 +0900

    directory fix

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-   res.render('index');
+app.get('/chat', (req, res) => {
+   res.render('chat');
 });
 
 
