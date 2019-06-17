dcf668ca90eb49002ddbcc06b965c9883e94d291
gwanwoodev <gwanwoodev@gmail.com>
2019-06-17 09:46:22 +0900

    update nofitication icons url

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,23 @@
+window.onload = () => {
+    if(window.Notification)
+        Notification.requestPermission();
+};
+
 const socket = io();
 
 socket.on('chatter', function(message) {
     $('#chat-messages').append($('<li>').text(message));
     $("#message").val("");
     $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+    
+    let notification = new Notification('SneakChat', {
+        icon: 'https://cdn1.iconfinder.com/data/icons/mix-color-3/502/Untitled-1-512.png',
+        body: message
+    });
+    
+    setTimeout(()=> {
+       notification.close(); 
+    }, 2000);
 });
 
 $('form').submit(function() {
@@ -13,3 +27,5 @@ $('form').submit(function() {
     socket.emit('chatter', `[${dateString}] : ${name} : ${message}`);
     return false;
 });
+
+
