d003b9c9097c2481a1592fa16d655557b1946ed8
gwanwoodev <devyeon1102@gmail.com>
2019-06-25 13:36:51 +0900

    Delete serviceworker check func

--- public/js/chat.js
+++ public/js/chat.js
@@ -68,19 +68,3 @@ function getUsers(connectList) {
 				
 	return templateheader + templates;
 }
-
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
-
-  navigator.serviceWorker.register('sw.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-
-    swRegistration = swReg;
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error);
-  });
-} else {
-  console.warn('Push messaging is not supported');
-}
